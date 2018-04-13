db1a658a3c1f8a22402b0e2b8f255edaefadd474
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-04-13 18:10:41 +0100

    Update regex to correctly handle dash

--- toc.rb
+++ toc.rb
@@ -12,7 +12,7 @@ File.open(filename, 'r') do |f|
     next if !line.start_with?('#') || forbidden_words.any? {|w| line =~ /#{w}/}
 
     title = line.gsub('#', '').strip
-    href = title.gsub(/[^a-zA-Z\d\s]/, '').gsub(' ', '-').downcase
+    href = title.gsub(/[^a-zA-Z\-\d\s]/, '').gsub(' ', '-').downcase
     puts '  ' * (line.count('#')-1) + "* [#{title}](\##{href})"
   end
 end
